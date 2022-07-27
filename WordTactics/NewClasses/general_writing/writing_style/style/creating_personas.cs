696357ad1ac0b715174b798a6c97632d1b7e95f3
JeremyBickel <102981806+JeremyBickel@users.noreply.github.com>
2022-07-27 14:01:51 -0400

    Delete WordTactics directory

--- WordTactics/NewClasses/general_writing/writing_style/style/creating_personas.cs
+++
@@ -1,9 +0,0 @@
-namespace WordTactics
-{
-internal class CreatingYourPersona
-{
-//Creating Your Persona&#160;
-//A writer&#8217;s persona communicates to their audience who they are. It&#8217;s&#160;not only&#160;a chance to&#160;give writing a little personality, it is also an opportunity for writers to&#160;connect to readers by allowing them to imagine the person who wrote the text they&#8217;re now reading (or the character narrating that text). A writer&#8217;s persona doesn&#8217;t need to&#160;encompass&#160;every single aspect of who they are as a person&#8212;rather,&#160;it&#8217;s a way of presenting a&#160;coherent identity to their readers through their language.&#160;
-//Consider how writers already establish different personas in&#160;everyday writing activities like texting and posting on social media.&#160;The way that&#160;someone texts their friends about going out for a party is likely different than how they text their grandmother about Thanksgiving dinner plans.&#160;Similarly, different social media platforms allow people to curate different personas with&#160;carefully selected&#160;posts and reposts of texts, images, videos, and gifs.&#160;For example, someone&#8217;s private Facebook account might&#160;express a&#160;different&#160;personality than&#160;the public LinkedIn account they use for work. Similarly, writers choose what persona to craft based on their goals and audience.&#160;
-}
-}
